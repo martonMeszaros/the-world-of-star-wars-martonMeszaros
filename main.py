e2ee69f6afc620c568dca4f1554d47ecade7497b
martonMeszaros <marton.m95@gmail.com>
2017-06-21 14:32:10 +0200

    Added login and register html files

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    if session.get('username'):
+        session.pop('username', None)
+    return redirect('/')
 
 
 @app.errorhandler(404)
